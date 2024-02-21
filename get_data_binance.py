1e226a3746a58828bf4bddbe59063eb49ca53a03
Zewil08 <will.moiroud@hotmail.fr>
2024-02-21 16:37:23 +0900

    added the weekly for binance data

--- get_data_binance.py
+++ get_data_binance.py
@@ -51,27 +51,18 @@ def getbinancedaily(start = "18 Aug 2017", sym = "BTCUSDT"):
     # df = df.iloc[1:]
     return df
 
-def getbinanceweekly(start = "18 Aug 2017", sym = "BTCUSDT"):
+def getbinanceweekly(start="18 Aug 2017", sym="BTCUSDT"):
     client = Client(API_KEY, API_SECRET)
+    # Utiliser l'intervalle hebdomadaire ici
     klines = client.get_historical_klines(sym, Client.KLINE_INTERVAL_1WEEK, start)
     df = pd.DataFrame(klines)
-    df = df.iloc[:,0:6]
+    df = df.iloc[:, 0:6]
     df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
-    df.index = [dt.datetime.fromtimestamp(int(x)/1000) for x in df.date]
+    df['date'] = pd.to_datetime(df['date'], unit='ms')
 
-    L = df['date'].values
-    L = list(range(L[0],L[-1]+1, 7*24*3600*1000))
-    for i,a in enumerate(L) :
-        L[i] = dt.datetime.fromtimestamp(a/1000)
-    df = df.reindex(L,method="ffill")
-
-    df = df.assign(date=L)
-    df = df_column_switch(df, 'close', 'volume')
-    df.drop(index=L[0], axis=0)
-    # df = df.iloc[1:]
     return df
 
 if __name__ == '__main__':
-    df = getbinancedaily()
+    df = getbinanceweekly()
     # df.to_csv('BTC_BinDaily.csv', index= False)
     df.to_csv('BTC_BinWeekly.csv', index= False)
